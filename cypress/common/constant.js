29163fe7027f37894e2d76aa753f2d71e872d86a
jhjang <jhjang@captv.co.kr>
2022-04-13 13:54:56 +0900

    test

--- cypress/common/constant.js
+++
@@ -1,6 +0,0 @@
-const IS_DEV = false
-export const TEST_URL = IS_DEV ? 'http://localhost' : 'http://www.wavve.com'
-//
-// const IS_DEV = true
-// export const TEST_URL = IS_DEV ? 'http://localhost:8080' : 'http://www.wavve.com'
-
